1890605bd32e669636320ab6f3f19722550f5a74
Luke Rodgers <lukeasrodgers@gmail.com>
2015-03-15 18:05:46 -0400

    parse ip address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::os;
+use std::net::{SocketAddr};
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = os::args();
+    let mut opts = Options::new();
+    let matches = match opts.parse(args.tail()) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string())
+    };
+    let addr_arg = matches.free[0].as_slice();
+    let addr:SocketAddr = FromStr::from_str(addr_arg.as_slice()).unwrap();
 }
